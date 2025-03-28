38813568339e820b77853fc222f814a3fddfd775
jovan <jovanpaloski@gmail.com>
2025-03-28 20:18:55 +0100

    Edit game functionality

--- client/src/components/game-edit/GameEdit.jsx
+++ client/src/components/game-edit/GameEdit.jsx
@@ -3,18 +3,26 @@ import { useNavigate, useParams } from "react-router"
 import gameService from "../../services/gameService.js";
 
 export default function GameEdit() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     const { gameId } = useParams();
     const [game, setGame] = useState({});
 
     useEffect(() => {
         gameService.getOne(gameId)
             .then(setGame);
-    }, []);
+    }, [gameId]);
+
+    const formAction = async (formData) => {
+        const gameData = Object.fromEntries(formData);
+
+        await gameService.edit(gameId, gameData);
+
+        navigate(`/games/${gameId}/details`);
+    }
 
     return (
         <section id="edit-page" className="auth">
-            <form id="edit">
+            <form id="edit" action={formAction}>
                 <div className="container">
 
                     <h1>Edit Game</h1>
